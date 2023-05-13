08806cff6a2683138e5815ca902a19e6f2f35ee4
Carlos Craig <carlos.craig09@gmail.com>
2023-05-13 11:14:42 -0700

    added listing route

--- index.js
+++ index.js
@@ -6,4 +6,11 @@ app.get("/", (req, res) => {
   res.send("Welcome to express");
 });
 
+app.get("/api/listing", (req, res) => {
+  res.send([
+    { id: 1, roomType: "Duplex" },
+    { id: 2, roomType: "Flat" },
+  ]);
+});
+
 app.listen(3000, () => console.log("Server is running on port 3000"));

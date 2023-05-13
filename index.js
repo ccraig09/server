5fa9f505860bf906cbd7cd4fb34b572d0a98c661
Carlos Craig <carlos.craig09@gmail.com>
2023-05-13 12:09:18 -0700

    express restapi

--- index.js
+++ index.js
@@ -2,15 +2,81 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+require("dotenv").config();
+
+const homes = [
+  {
+    id: 1,
+    type: "Apartment",
+    description: "Well furnished apartment",
+  },
+  {
+    id: 2,
+    type: "Flat",
+    description: "Well furnished apartment at flatville",
+  },
+];
+
 app.get("/", (req, res) => {
   res.send("Welcome to express");
 });
 
 app.get("/api/listing", (req, res) => {
-  res.send([
-    { id: 1, roomType: "Duplex" },
-    { id: 2, roomType: "Flat" },
-  ]);
+  res.send(homes);
 });
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+app.get("/api/listing/:id", (req, res) => {
+  const home = homes.find((home) => home.id === parseInt(req.params.id));
+
+  if (!home) {
+    res.status(404).send("The home with the given ID is not found");
+  }
+
+  res.send(home);
+});
+
+app.post("/api/listing", (req, res) => {
+  if (!req.body.type || !req.body.description) {
+    return res.status(400).send("Title and description is required");
+  }
+
+  const home = {
+    id: homes.length + 1,
+    type: req.body.type,
+    description: req.body.description,
+  };
+
+  homes.push(home);
+  res.send(home);
+});
+
+app.put("/api/listing/:id", (req, res) => {
+  const home = homes.find((home) => home.id === parseInt(req.params.id));
+
+  if (!home) {
+    return res.status(404).send("The home with the given ID is not found");
+  }
+
+  home.type = req.body.type;
+  home.description = req.body.description;
+
+  res.send(home);
+});
+
+app.delete("/api/listing/:id", (req, res) => {
+  const home = homes.find((home) => home.id === parseInt(req.params.id));
+
+  if (!home) {
+    return res.status(404).send("The home with the given ID is not found");
+  }
+
+  const index = homes.indexOf(home);
+  homes.splice(index, 1);
+  res.send(home);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
